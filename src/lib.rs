ead4413d5ac839130e641f8372f5c39e68607324
David Barnett <david@barnett.net.nz>
2015-09-08 22:02:33 +1200

    Split into library and executable
    Renamed Symbol to start to follow rust's style guide

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+#[derive(Debug)]
+pub enum Symbol {
+    Add,
+    Sub,
+    Next,
+    Prev,
+    WhileStart,
+    WhileEnd,
+    Print,
+    Get,
+}
+
+pub fn parse(code: String) -> Vec<Symbol> {
+
+    let mut program = Vec::new();
+    for c in code.chars() {
+        let symbol = match c {
+            '+' => Symbol::Add,
+            '-' => Symbol::Sub,
+            '>' => Symbol::Next,
+            '<' => Symbol::Prev,
+            '[' => Symbol::WhileStart,
+            ']' => Symbol::WhileEnd,
+            '.' => Symbol::Print,
+            ',' => Symbol::Get,
+            _ => continue, // ignore every other character
+        };
+        println!("{} {:?}", c, symbol);
+        program.push(symbol);
+    }
+    println!("{:?}\n", program);
+    program
+}
+
+pub fn run(program: &Vec<Symbol>, inital: Option<Vec<u8>>) {
+
+    let mut data = match inital {
+        Some(init) => init.clone(),
+        None => vec![0,0,0,0,0,0,0,0],
+    };
+
+    let mut iter = 0;
+    let mut cell = *data.get_mut(iter).unwrap();
+    for sym in program {
+        match *sym {
+            Symbol::Add => cell = if cell == std::u8::MAX {
+                std::u8::MIN
+            } else {
+                cell + 1
+            },
+            Symbol::Sub => cell = if cell == std::u8::MIN {
+                std::u8::MAX
+            } else {
+                cell - 1
+            },
+            Symbol::Next => cell = match data.get_mut(iter + 1) {
+                Some(n) => {iter += 1; *n},
+                None => cell,
+            },
+            Symbol::Prev => cell = match data.get_mut(iter - 1) {
+                Some(n) => {iter -= 1; *n},
+                None => cell,
+            },
+            Symbol::WhileStart => (),
+            Symbol::WhileEnd => (),
+            Symbol::Print => print!("{}", cell as char),
+            Symbol::Get => (),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn no_arthimatic_underflow() {
+        run(&parse("-".to_string()), None);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,12 @@
+extern crate bf;
 
-#[derive(Debug)]
-enum BFSymbol {
-    Add,
-    Sub,
-    Next,
-    Prev,
-    WhileStart,
-    WhileEnd,
-    Print,
-    Get,
-}
+#[cfg(not(test))]
+use bf::*;
 
-fn parse(code: String) -> Vec<BFSymbol> {
-
-    let mut program = Vec::new();
-    for c in code.chars() {
-        let symbol = match c {
-            '+' => BFSymbol::Add,
-            '-' => BFSymbol::Sub,
-            '>' => BFSymbol::Next,
-            '<' => BFSymbol::Prev,
-            '[' => BFSymbol::WhileStart,
-            ']' => BFSymbol::WhileEnd,
-            '.' => BFSymbol::Print,
-            ',' => BFSymbol::Get,
-            _ => continue, // ignore every other character
-        };
-        println!("{} {:?}", c, symbol);
-        program.push(symbol);
-    }
-    println!("{:?}", program);
-    program
-}
-
-fn run(program: &Vec<BFSymbol>, inital: Option<Vec<u8>>) {
-
-    let mut data = match inital {
-        Some(init) => init.clone(),
-        None => vec![0,0,0,0,0,0,0,0],
-    };
-
-    let mut iter = 0;
-    let mut cell = *data.get_mut(iter).unwrap();
-    for sym in program {
-        match *sym {
-            BFSymbol::Add => cell = if cell == std::u8::MAX {
-                std::u8::MIN
-            } else {
-                cell + 1
-            },
-            BFSymbol::Sub => cell = if cell == std::u8::MIN {
-                std::u8::MAX
-            } else {
-                cell - 1
-            },
-            BFSymbol::Next => cell = match data.get_mut(iter + 1) {
-                Some(n) => {iter += 1; *n},
-                None => cell,
-            },
-            BFSymbol::Prev => cell = match data.get_mut(iter - 1) {
-                Some(n) => {iter -= 1; *n},
-                None => cell,
-            },
-            BFSymbol::WhileStart => (),
-            BFSymbol::WhileEnd => (),
-            BFSymbol::Print => print!("{}", cell as char),
-            BFSymbol::Get => (),
-        }
-    }
-}
-
+#[cfg(not(test))]
 fn main() {
     run(&parse(".".to_string()), None);
-    run(&parse("+++.>.".to_string()), None);
+    run(&parse("+.".to_string()), None);
+    run(&parse("-".to_string()), None);
 }
+
