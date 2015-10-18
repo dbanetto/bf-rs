b430ffccb4d82d6d2db2ba3a3617fc183a94f5a0
David Barnett <david@barnett.net.nz>
2015-10-19 09:32:25 +1300

    Start of CLI
    Added clap-rs for cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 extern crate bf;
+extern crate clap;
 
 #[cfg(not(test))]
 use bf::*;
+use clap::{Arg, App, SubCommand};
 
 #[cfg(not(test))]
 fn main() {
-    BFProgram::parse(".".to_string()).unwrap().run();
-    print!("\n");
-    BFProgram::parse(",.".to_string()).unwrap().run(); // FIXME:
-    print!("\n");
-    BFProgram::parse("+.".to_string()).unwrap().run();
-    print!("\n");
-    BFProgram::parse("-".to_string()).unwrap().run();
-    print!("\n");
-    BFProgram::parse("[-]".to_string()).unwrap().run();
-    BFProgram::parse("++++>++".to_string()).unwrap().run();
+    let matches = App::new("bfi")
+                    .version("0.1.0")
+                    .author("")
+                    .about("")
+                    .arg(Arg::with_name("CODE")
+                        .short("c")
+                        .help("brainfuck code to execute")
+                        .takes_value(true))
+                    .arg(Arg::with_name("FILE")
+                        .short("f")
+                        .help("brainfuck code to execute from file")
+                        .takes_value(true))
+                    .arg(Arg::with_name("INPUT")
+                        .short("i")
+                        .long("input")
+                        .help("Read input from file instead of stdin")
+                        .takes_value(true))
+                    .get_matches();
+    // Hello world from wikipedia
+    BFProgram::parse("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.".to_string()).unwrap().run();
 }
 
